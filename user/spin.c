1c08209559c65ff6dd78ad491ffed1aef137ead6
aristotle0x01 <aristotle0x01@protonmail.com>
2023-08-07 10:36:14 +0800

    lab4: minor, make grade work

--- user/spin.c
+++ user/spin.c
@@ -8,9 +8,9 @@ umain(int argc, char **argv)
 {	
 	envid_t env;
 
-	cprintf("I am the parent [%08x].  Forking the child...\n", thisenv->env_id);
+	cprintf("I am the parent.  Forking the child...\n", thisenv->env_id);
 	if ((env = fork()) == 0) {
-		cprintf("I am the child [%08x] [%08x].  Spinning...\n", thisenv->env_id, thisenv->env_tf.tf_eflags);
+		cprintf("I am the child.  Spinning...\n", thisenv->env_id, thisenv->env_tf.tf_eflags);
 		while (1)
 			/* do nothing */;
 	}
